673e8fe39eacf94d5bd5dbde72c4cc4a3a26b593
Simon <sstoye@simonstoye.de>
2020-06-09 08:50:36 +0200

    Fixed active style

--- src/vue-virtual-table.vue
+++ src/vue-virtual-table.vue
@@ -347,7 +347,7 @@
               @click="handleClickItem(props.item)"
               :class="{
                 selected: props.item._eSelected,
-                active: props.item._eTag == selectedTag,
+                active: props.item._eId == selectedTag,
                 unselectable: !selectable,
                 'item-line-allow-hightlight': hoverHighlight
               }"
